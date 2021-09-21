e8cfa416d95581aa69e636584debf97416a3246c
andres <afyaruro@unicesar.edu.co>
2021-09-21 15:38:30 -0500

    aprende y diviertete con las matematicas

---
+++ Matematicas/Operaciones/operacione.js
@@ -0,0 +1,8 @@
+function operaciones(){
+
+    var numero1 = document.getElementById("numero1").value;
+    var numero2 = document.getElementById("numero2").value;
+    var resultado = parseInt(numero1) + parseInt(numero2);
+
+    alert(resultado);
+}
